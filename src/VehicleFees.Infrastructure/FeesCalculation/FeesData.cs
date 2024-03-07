49a1e044067c8346575113e91918b5fa6bff6a5c
Ricardo Toscano <ricardo@wilqo.com>
2024-03-07 13:43:38 -0500

    Using static property when we get the data

--- src/VehicleFees.Infrastructure/FeesCalculation/FeesData.cs
+++ src/VehicleFees.Infrastructure/FeesCalculation/FeesData.cs
@@ -5,45 +5,40 @@ namespace VehicleFees.Infrastructure.FeesCalculation;
 
 internal class FeesData
 {
-    public static List<FeesCost> GetDefaultFees()
-    {
-        var feesList = new List<FeesCost>
+    public static List<FeesCost> FeeCostData =
+    [
+        new FeesCost
         {
-            new FeesCost
+            MinimumBasicFee = 10,
+            MaximumBasicFee = 50,
+            BaseFeePercentage = 0.1m,
+            SpecialFeePercentage = 0.02m,
+            StorageFee = 100,
+            VehicleType = VehicleType.Common,
+            AssociatedFee = 
             {
-                MinimumBasicFee = 10,
-                MaximumBasicFee = 50,
-                BaseFeePercentage = 0.1m,
-                SpecialFeePercentage = 0.02m,
-                StorageFee = 100,
-                VehicleType = VehicleType.Common,
-                AssociatedFee = new Dictionary<decimal, decimal>
-                {
-                    { 500, 5 },
-                    { 1000, 10 },
-                    { 3000, 15 },
-                    { decimal.MaxValue, 20 }
-                }
-
-            },
-            new FeesCost
-            {
-                MinimumBasicFee = 25,
-                MaximumBasicFee = 200,
-                BaseFeePercentage = 0.1m,
-                SpecialFeePercentage = 0.04m,
-                StorageFee = 100,
-                VehicleType = VehicleType.Luxury,
-                AssociatedFee = new Dictionary<decimal, decimal>
-                {
-                    { 500, 5 },
-                    { 1000, 10 },
-                    { 3000, 15 },
-                    { decimal.MaxValue, 20 }
-                }
+                { 500, 5 },
+                { 1000, 10 },
+                { 3000, 15 },
+                { decimal.MaxValue, 20 }
             }
-        };
 
-        return feesList;
-    }
+        },
+        new FeesCost
+        {
+            MinimumBasicFee = 25,
+            MaximumBasicFee = 200,
+            BaseFeePercentage = 0.1m,
+            SpecialFeePercentage = 0.04m,
+            StorageFee = 100,
+            VehicleType = VehicleType.Luxury,
+            AssociatedFee = new()
+            {
+                { 500, 5 },
+                { 1000, 10 },
+                { 3000, 15 },
+                { decimal.MaxValue, 20 }
+            }
+        }
+    ];
 }
--- src/VehicleFees.Infrastructure/FeesCalculation/FeesRepository.cs
+++ src/VehicleFees.Infrastructure/FeesCalculation/FeesRepository.cs
@@ -7,16 +7,11 @@ namespace VehicleFees.Infrastructure.FeesCalculation;
 
 public class FeesRepository : IFeesRepository
 {
-    private List<FeesCost> _feesList;
-    public FeesRepository()
-    {
-        _feesList = FeesData.GetDefaultFees();
-    }
     public async Task<FeesCost> GetFeesByVehicleType(VehicleType vehicleType, CancellationToken cancellationToken)
     {
         await Task.Delay(2000);  //Simulate any IO process.
         
-        var feesCost = _feesList.FirstOrDefault(fee => fee.VehicleType == vehicleType);
+        var feesCost = FeesData.FeeCostData.FirstOrDefault(fee => fee.VehicleType == vehicleType);
         if (feesCost is null) 
         {
             throw new FeeNotFound($"Fees not found for the '{vehicleType}' vehicle type in the FeesRepository repository.");
